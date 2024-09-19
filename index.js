a28bf9c6e6081501397ab1f20cca27132fb6f769
ayan471 <pakhiraayan@gmail.com>
2024-09-19 12:50:18 +0530

    fixing bug

--- index.js
+++ index.js
@@ -6,51 +6,47 @@ import cors from "cors";
 const app = express();
 import apiRouter from "./routes/api.js";
 
+app.use(express.json());
+
 app.use(
-    cors({
-      origin: "*",
-      methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-      credentials: true,
-    })
-  );
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
-  
-app.get('/data',(req,res)=>{
-    console.log("called");
-    const accessToken = 'acess token'; // Use your Admin API access token
-  const shopName = 'Taare'; // E.g., 'yourshopname'
-  
-  const url = `https://${shopName}.myshopify.com/admin/api/2024-07/orders.json`;
-  
-  const response = axios.get(url, {
-    headers: {
-      'X-Shopify-Access-Token': accessToken,
-    },
-    params: {
-      status: 'any',
-      fields: 'id,created_at,total_price,note_attributes',
-    },
-  });
-  
-console.log("orders", response.data.orders);
-
+app.get("/data", (req, res) => {
+  //     console.log("called");
+  //     const accessToken = 'acess token'; // Use your Admin API access token
+  //   const shopName = 'Taare'; // E.g., 'yourshopname'
+  //   const url = `https://${shopName}.myshopify.com/admin/api/2024-07/orders.json`;
+  //   const response = axios.get(url, {
+  //     headers: {
+  //       'X-Shopify-Access-Token': accessToken,
+  //     },
+  //     params: {
+  //       status: 'any',
+  //       fields: 'id,created_at,total_price,note_attributes',
+  //     },
+  //   });
+  // console.log("orders", response.data.orders);
   // Parse the note_attributes to find the referrer for each order
-//   const ordersWithReferrers = response.data.orders.map(order => {
-//     const referrer = order.note_attributes.find(attr => attr.name === 'referrer');
-//     return {
-//       order_id: order.id,
-//       created_at: order.created_at,
-//       total_price: order.total_price,
-//       referrer: referrer ? referrer.value : 'N/A',
-//     };
-//   });
-
+  //   const ordersWithReferrers = response.data.orders.map(order => {
+  //     const referrer = order.note_attributes.find(attr => attr.name === 'referrer');
+  //     return {
+  //       order_id: order.id,
+  //       created_at: order.created_at,
+  //       total_price: order.total_price,
+  //       referrer: referrer ? referrer.value : 'N/A',
+  //     };
+  //   });
   // Return the JSON result
-//   return res.json(ordersWithReferrers);
-})
+  //   return res.json(ordersWithReferrers);
+});
 const PORT = process.env.PORT || 8080;
 
-app.use("/",apiRouter);
+app.use("/", apiRouter);
 let server;
 Promise.all([mongo()])
   .then(() => {
