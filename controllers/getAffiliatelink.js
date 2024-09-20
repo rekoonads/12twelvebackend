c9284bc8a248fc0517a5d8bcbd6250691b8ed572
ayan471 <pakhiraayan@gmail.com>
2024-09-20 14:24:34 +0530

    fixing shopify

--- controllers/getAffiliatelink.js
+++ controllers/getAffiliatelink.js
@@ -1,18 +1,21 @@
-import Affiliate from "../models/Affiliate.js";
 export default async (req, res) => {
   try {
     const val = req.query.val;
-    console.log("searched val : - ", val)
+    console.log("searched val : - ", val);
+
     const affiliate = await Affiliate.findOneAndUpdate(
       { generatedVal: val },
-      { $inc: { impression: 1 } }, 
-      { new: true } 
+      { $inc: { impression: 1 } },
+      { new: true }
     );
 
-    
-    if (!affiliate) return res.status(404).send('Affilaite link not found');
-    res.redirect(affiliate.redirectLink);
-    // res.json(affiliate);
+    if (!affiliate) return res.status(404).send("Affiliate link not found");
+
+    // Add UTM parameters for better tracking on Shopify
+    const redirectUrl = `${affiliate.redirectLink}?utm_source=https://www.my12twelve.com/&utm_medium=referral&utm_campaign=affiliate&utm_referrer=https://www.my12twelve.com/`;
+
+    console.log("Redirecting to:", redirectUrl);
+    res.redirect(redirectUrl);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
