15eb13693d3e504f871552b40279fcbfbe635fce
shibu-g <shibugoper1@gmail.com>
2024-10-01 17:51:50 +0530

    added user

--- models/Affiliate.js
+++ models/Affiliate.js
@@ -4,13 +4,17 @@ const affiliateSchema = new mongoose.Schema({
   generatedVal: String,
   redirectLink: String,
   impression: { type: Number, default: 0 },
+  userId: String,
   redirects: [
     {
       uniqueId: String,
       timestamp: Date,
     },
   ],
-});
+}, 
+{
+  timestamps: true,
+},);
 
 const Affiliate = mongoose.model("Affiliate", affiliateSchema);
 
