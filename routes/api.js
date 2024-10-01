4f6005b3c19563ebba145879b07d5c2ddab8ade5
shibu-g <shibugoper1@gmail.com>
2024-10-01 16:53:48 +0530

    get all

--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,17 @@ import { Router } from "express";
 import getAffiliatelink from "../controllers/getAffiliatelink.js";
 import addAffiliatelink from "../controllers/addAffiliatelink.js";
 import userCreate from "../controllers/userCreate.js";
+import { userModel } from "../models/User.js";
 
 const router = Router();
 
 router.get('/affiliate',getAffiliatelink);
 router.post('/add-affiliate',addAffiliatelink);
 router.post("/webhook/user-created", userCreate);
+router.get("/get-all-user",async (req,res)=>{
+    
+    const users = await userModel.find({});
+    return res.json(users);
+});
 
 export default router;
