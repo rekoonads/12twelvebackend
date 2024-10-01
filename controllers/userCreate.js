ae56047ab5c5d15decd51ace8ecc0d0f8d7a9def
shibu-g <shibugoper1@gmail.com>
2024-10-01 12:38:04 +0530

    added user

---
+++ controllers/userCreate.js
@@ -0,0 +1,40 @@
+import { userModel } from '../models/User.js';
+
+export default async (req, res) => {
+    try {
+        const {
+            id,
+            first_name,
+            last_name,
+            email_addresses
+        } = req.body.data;
+
+        const new_user = new userModel({
+            userId: id,
+            firstName: first_name,
+            lastName: last_name,
+            userType: "influencer",
+            email: email_addresses[0].email_address,
+            phoneNo: ""
+        });
+
+        console.log(new_user);
+        let response_data = [];
+
+        const user = await userModel.findOne({ userId: id })
+
+        if (user) {
+            console.log(`User with ID ${id} already exists.`);
+            return res.status(409).json({ error: `User with ID ${id} already exists.` });
+        } else {
+            const createdUser = await userModel.create(new_user);
+            response_data.push({ user_data: createdUser });
+        }
+
+        console.log(response_data);
+        return res.status(201).json(response_data);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import getAffiliatelink from "../controllers/getAffiliatelink.js";
 import addAffiliatelink from "../controllers/addAffiliatelink.js";
+import userCreate from "../controllers/userCreate.js";
 
 const router = Router();
 
-router.get('/affiliate',getAffiliatelink)
-router.post('/add-affiliate',addAffiliatelink)
+router.get('/affiliate',getAffiliatelink);
+router.post('/add-affiliate',addAffiliatelink);
+router.post("/webhook/user-created", userCreate);
 
 export default router;
