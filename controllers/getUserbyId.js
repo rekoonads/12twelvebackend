537846d01be701245593cddf95db77acc19c4740
ayan471 <pakhiraayan@gmail.com>
2024-10-04 16:38:18 +0530

    added fix

--- controllers/getUserbyId.js
+++ controllers/getUserbyId.js
@@ -5,7 +5,7 @@ export default async (req, res) => {
     const userId = req.query.userId;
     console.log(userId);
     const user = await userModel.findOne({ userId });
-    return res.status(500).json(user);
+    return res.status(200).json(user);
   } catch (error) {
     console.error("Error getting user:", error);
     return res.status(500).json({ message: "Internal server error" });
